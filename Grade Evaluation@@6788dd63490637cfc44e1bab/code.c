ecce7ae3b4619add31ecf344fdb3b58e491148b1
sehaj125 <sehajpreet1669.becse24@chitkara.edu.in>
2025-02-18 13:40:03 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -2,26 +2,20 @@
 int main(){
     char grade;
     scanf("%c",&grade);
-
-    switch (grade){
-        case 'A':
-            printf("Excellent");
-            break;
-        case 'B':
-            printf("Good");
-            break;
-        case 'C';
-            printf("Average");
-            break;
-        case 'D':
-            printf("Below Average");
-            break;
-        case 'F';
-            printf("Fail");
-            break;
-        default:
-            printf("Invalid grade");
-            break;
+    if(grade == 'A'){
+        printf("Excellent");
+    }
+    else if(grade == 'B'){
+        printf("Good");
+    }
+    else if(grade == 'C'){
+        printf("Average");
+    }
+    else if(grade == 'D'){
+        printf("Below Average");
+    }
+    else{
+        printf("Fail");
     }
     return 0;
 }
